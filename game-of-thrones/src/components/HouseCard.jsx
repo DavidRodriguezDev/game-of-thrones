44b222cda269c632b1f073325ef159497e7321ba
DavidRodriguezDev <contacto.davidrodriguezdiaz@gmail.com>
2022-11-09 21:34:06 +0100

    Added details pages funcionalities

--- game-of-thrones/src/components/HouseCard.jsx
+++ game-of-thrones/src/components/HouseCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './HouseCard.scss'
 
 
 export const HousesCard = ({houses}) => {
  
+    const navigate = useNavigate()
   
     return (
     <div className="container-fluid d-flex flex-wrap gap-3 houses-gallery">
         {houses.map((item) => 
     
-                <div className='card-shield' key={item.id}>
+                <div onClick={()=> navigate(`/houses/${item.name}`)} className='card-shield' key={item.id}>
                 <div className='w-100 card-shield--div-img'>
                     <img src={item.logoURL?item.logoURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png"} alt={item.name}></img>
                 </div>
