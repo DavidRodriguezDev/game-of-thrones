a66c97822848c5c2cc11b49c3b742e8c12fb9419
Dorian <112967549+imDorian@users.noreply.github.com>
2022-11-09 16:32:35 +0100

    Update CharacterCard.jsx

--- game-of-thrones/src/components/CharacterCard.jsx
+++ game-of-thrones/src/components/CharacterCard.jsx
@@ -7,7 +7,6 @@ export const CharacterCard = ({characters}) => {
     <div className='container-fluid d-flex flex-wrap gap-4 characters-gallery '>
         {characters.map((item)=> 
         <div className='card-item' key={item.id}>
-            <div>hola</div>
             <img src={item.image}></img>
             <div className='rectangle'>
             <span className='name'>{item.name}</span>
