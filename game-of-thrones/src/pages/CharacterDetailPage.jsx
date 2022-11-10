31a78d920a19f8b146ac64bbc48bd4c2633e4a97
DavidRodriguezDev <contacto.davidrodriguezdiaz@gmail.com>
2022-11-10 18:30:59 +0100

    changes

--- game-of-thrones/src/pages/CharacterDetailPage.jsx
+++ game-of-thrones/src/pages/CharacterDetailPage.jsx
@@ -19,16 +19,21 @@ export const CharacterDetailPage = () => {
     }, []) 
 
     return (
-    <div>
+    <div className='b-charactersDetail__container'>
         <ComeBackButtonCh></ComeBackButtonCh>
-        <img src={character.image} alt={character.name}></img>
-        <h2>{character.name}</h2>
-        <p>{character.house}</p>
-        <p>{character.allegiances}</p>
-        <p>{character.appearances}</p>
-        <p>{character.father}</p>
-        <p>{character.siblings}</p>
-        <p>{character.titles}</p>
+        <div className='b-charactersDetail__image'>
+            <img src={character.image} alt={character.name}></img>
+            <h2>{character.name}</h2>
+        </div>
+        <div className='b-charactersDetail__info'>
+            <p>{character.house}</p>
+            <p>{character.allegiances}</p>
+            <p>{character.appearances}</p>
+            <p>{character.father}</p>
+            <p>{character.siblings}</p>
+            <p>{character.titles}</p>
+        </div>    
+
     </div>
   )
 }
