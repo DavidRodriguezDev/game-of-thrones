10ecd70408d2b3bac972dd830a5d14e2c406f74e
i-am-iron-man <iron-man@avengers.com>
2022-11-11 12:03:33 +0100

    cronologia-2

--- game-of-thrones/src/components/Chronology.jsx
+++ game-of-thrones/src/components/Chronology.jsx
@@ -50,7 +50,7 @@ export const Chronology = () => {
             return (
               <>
                   <div className="b-chronology--card" key={index} style={index % 2 ?
-              { "margin-top": "150px", "margin-left": "-1.5px", "border-left": "1px solid white" } :
+              { "margin-top": "150px", "margin-left": "-1.5px","padding-top":"200px", "height" : "150px", "border-left": "1px solid white" } :
               { "margin-top": "0px", "border-right": "1px solid white" }}>
                     <h3 className='b-chronology--card__age'>{element.age.age}</h3>
                     <h2 className="b-chronology--card__name">{element.name}</h2>
