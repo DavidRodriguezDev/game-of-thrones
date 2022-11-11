1da4978d6867553b1330c190a1f96901d67c7464
DavidRodriguezDev <contacto.davidrodriguezdiaz@gmail.com>
2022-11-11 12:35:53 +0100

    Home position fixed

--- game-of-thrones/src/pages/CharacterDetailPage.jsx
+++ game-of-thrones/src/pages/CharacterDetailPage.jsx
@@ -5,25 +5,6 @@ import {DetailCharacter} from '../components/DetailCharacter'
 export const CharacterDetailPage = () => {
   
     return (
-<<<<<<< HEAD
-    <div className='b-charactersDetail__container'>
-        <ComeBackButtonCh></ComeBackButtonCh>
-        <div className='b-charactersDetail__image'>
-            <img src={character.image} alt={character.name}></img>
-            <h2>{character.name}</h2>
-        </div>
-        <div className='b-charactersDetail__info'>
-            <p>{character.house}</p>
-            <p>{character.allegiances}</p>
-            <p>{character.appearances}</p>
-            <p>{character.father}</p>
-            <p>{character.siblings}</p>
-            <p>{character.titles}</p>
-        </div>    
-
-    </div>
-=======
     <DetailCharacter></DetailCharacter>
->>>>>>> dev
   )
 }
