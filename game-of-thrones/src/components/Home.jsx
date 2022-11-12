47afa3e4907fe2d959473a01816499f1f5a52aaf
imDorian <dorian.cmd01@gmail.com>
2022-11-12 03:46:37 +0100

    lenguages bar ok--- proyect 90%

--- game-of-thrones/src/components/Home.jsx
+++ game-of-thrones/src/components/Home.jsx
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
 import { MyLangContext } from '../context/MyLangContext';
 import "./Home.scss"
+import { LenguageSelect } from './LenguageSelect';
 
 export const Home = () => {
 
   const {t} = useContext(MyLangContext)
 
   return (
+  <>
+ 
     <div className='b-home'>
-      <div className='b-home__title'>{t('title')}</div>
+
+      <div className='container-fluid d-flex justify-content-end align-items-center gap-4 b-home--lang'>
+      <LenguageSelect></LenguageSelect>
+      </div>
+      <div className='b-home--title-box'>
+        <div className='b-home--title-box__title'>{t('title')}</div>
+        </div>
+
     </div>
+    </>
   )
 }
